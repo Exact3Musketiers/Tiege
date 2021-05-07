861855199e3e37ba4abc4120c3304abefd8298af
Wim <w.j.stienstra@home.nl>
2021-05-07 16:23:22 +0200

    Added lyrics button in widget

--- resources/views/partials/music.blade.php
+++ resources/views/partials/music.blade.php
@@ -6,5 +6,6 @@
         <img src={{ $recentTracks->track[0]->image[1]->{"#text"} }}/>
         <h3>{{ $recentTracks->track[0]->name }}</h3>
         <h6>{{ $recentTracks->track[0]->album->{'#text'} }}</h6>
+        <a href="{{route('lyrics')}}">See lyrics</a>
     </div>
 @endif
