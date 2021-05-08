1f404280b1114b4d188bd779ae060748dd74c7f6
Wim <w.j.stienstra@home.nl>
2021-05-08 18:05:24 +0200

    Made the sidebar sticky

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,6 +26,8 @@
         <div class="container-fluid">
             <div class="row flex-nowrap">
                 <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark sb-with">
+                </div>
+                <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark sb-with position-fixed">
                     <div
                         class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 min-vh-100">
                         <a href="{{ route('home') }}"
