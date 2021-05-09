a4d60b8d8d5fdd5dd87db75084b2b12098bb60a8
Piet <pkorfmaker@gmail.com>
2021-05-09 17:17:22 +0200

    Search websites from home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,11 +26,49 @@ class HomeController extends Controller
         return view('home');
     }
 
+    /**
+     * Search the internet.
+     *
+     *
+     */
     public function search(Request $request)
     {
+        // Make google search
         $search = str_replace(' ', '+', $request->search);
         $url = 'https://www.google.com/search?q=' . $search;
+        // Check if request contains a dot
+        if (strpos($request->search, '.') != null)
+        {
+            // Get top level domain
+            $site = $request->search;
+            $tld = explode(".", parse_url($site, PHP_URL_HOST));
+            // checks if request contains space
+            if (!str_contains($site, ' '))
+            {
+                // Check if request contains top level domain
+                if (count($tld) > 0);
+                {
+                    // Removes aal domain prefixes
+                    $site = str_replace('https://', '', $site);
+                    $site = str_replace('http://', '', $site);
+                    $site = str_replace('www.', '', $site);
 
-        return redirect()->away($url);
+                    // Adds https:// to domain name
+                    $site = 'https://' . $site;
+                    // Opens site
+                    return redirect()->away($site);
+                }
+            }
+            else
+            {
+                // Searches google
+                return redirect()->away($url);
+            }
+        }
+        else
+        {
+            // Searches google
+            return redirect()->away($url);
+        }
     }
 }
