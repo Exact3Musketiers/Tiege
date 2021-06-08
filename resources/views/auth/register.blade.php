b1a5972f5a898ebe634d4e2b51ce16c2e32ff623
Piet <pkorfmaker@gmail.com>
2021-06-08 22:05:27 +0200

    Register fix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -17,7 +17,7 @@
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="hoi{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -31,7 +31,7 @@
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Lastfm Username') }}</label>
 
                             <div class="col-md-6">
-                                <input id="lastfm" type="text" class="form-control @error('lastfm') is-invalid @enderror" name="lastfm" value="Pietj_{{ old('lastfm') }}" autocomplete="lastfm" autofocus>
+                                <input id="lastfm" type="text" class="form-control @error('lastfm') is-invalid @enderror" name="lastfm" value="{{ old('lastfm') }}" autocomplete="lastfm" autofocus>
 
                                 @error('lastfm')
                                 <span class="invalid-feedback" role="alert">
@@ -45,7 +45,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="2@2{{ old('email') }}" required autocomplete="email">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -59,7 +59,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" value="12345678" required autocomplete="new-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -73,7 +73,7 @@
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" value="12345678" required autocomplete="new-password">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
 
