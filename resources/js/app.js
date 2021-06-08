5a6b8d2d918959f76cd0214c4c718a0ed82914c2
Piet <pkorfmaker@gmail.com>
2021-06-08 20:52:15 +0200

    Registration for super musketiers

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+
+var i = 0;
+counter  = function() {
+    i++;
+    if (i === 10) {
+        document.getElementById('musketier').classList.remove('hidden-input')
+    }
+}
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card bg-dark">
-                <div class="card-header">{{ __('Register') }}</div>
-
+                <div class="card-header">
+                    <h2 class="user-select-none" onclick="counter()">{{ __('Register') }}</h2>
+                </div>
+                <hr class="m-0">
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
@@ -15,7 +17,7 @@
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="hoi{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -29,7 +31,7 @@
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Lastfm Username') }}</label>
 
                             <div class="col-md-6">
-                                <input id="lastfm" type="text" class="form-control @error('lastfm') is-invalid @enderror" name="lastfm" value="{{ old('lastfm') }}" autocomplete="lastfm" autofocus>
+                                <input id="lastfm" type="text" class="form-control @error('lastfm') is-invalid @enderror" name="lastfm" value="Pietj_{{ old('lastfm') }}" autocomplete="lastfm" autofocus>
 
                                 @error('lastfm')
                                 <span class="invalid-feedback" role="alert">
@@ -43,7 +45,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="2@2{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -57,7 +59,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" value="12345678" required autocomplete="new-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -71,7 +73,15 @@
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" value="12345678" required autocomplete="new-password">
+                            </div>
+                        </div>
+
+                        <div id="musketier" class="form-group row hidden-input">
+                            <label for="musketier-password" class="col-md-4 col-form-label text-md-right">{{ __('Musketier Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="musketier-password" type="password" class="form-control" name="password_musketier">
                             </div>
                         </div>
 
