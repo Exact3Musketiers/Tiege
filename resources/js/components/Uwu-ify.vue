428e9378281d738219ae6590e41aeb6e6c7171a1
Nanco Feenstra <48758689+Nancofeenstra@users.noreply.github.com>
2021-12-02 13:43:38 +0100

    Fix copy and improve notification

--- resources/js/components/Uwu-ify.vue
+++ resources/js/components/Uwu-ify.vue
@@ -2,45 +2,32 @@
     <div class="row mt-5">
         <div class="card bg-dark col-md-12">
             <div class="card-header">UwUify</div>
+            <div class="alert alert-primary d-flex align-items-center" role="alert" v-if="showNotification">
+                <i class="fs-5 fad fa-copy flex-shrink-0 me-2"></i>
+            <div>
+                Text gekopieewd. *aaaargg* UwU
+            </div>
+            </div>
             <div class="card-body">
                 <div class="mb-3">
                     <label for="text" class="form-label">Je text</label>
-                    <textarea class="form-control form-control-lg" id="uwuin" rows="3" placeholder="UwU in" v-model="textToUwuIfy"></textarea>
+                    <textarea class="form-control form-control-lg textarea-height" id="uwuin" rows="3" placeholder="UwU in" v-model="textToUwuIfy"></textarea>
                 </div>
                 <label for="text" class="form-label">Je UwU</label>
                 <div class="input-group mb-3">
-                    <textarea class="form-control form-control-lg" id="uwuout" rows="3"
+                    <textarea class="form-control form-control-lg textarea-height" id="uwuout" rows="3"
                         placeholder="UwU out" v-model="textUwuFied"></textarea>
                     <button class="btn btn-secondary" type="button" v-on:click="saveToClipboard"><i
                             class="far fa-copy"></i></button>
                 </div>
-                <!-- <button class="btn btn-primary">UwUify!</button> -->
             </div>
         </div>
     </div>
-<div class="alert alert-primary d-flex align-items-center" role="alert" v-if="showNotification">
-    <i class="fs-5 fad fa-copy flex-shrink-0 me-2"></i>
-  <div>
-    Text gekopieewd. *aaaargg* UwU
-  </div>
-</div>
 </template>
 
 <style>
-.xd {
-  width: 100px;
-  height: 100px;
-  position: relative;
-  animation-name: example;
-  animation-duration: 4s;
-}
-
-@keyframes example {
-  0%   {left:0px; top:0px;}
-  25%  {left:200px; top:0px;}
-  50%  {left:200px; top:200px;}
-  75%  {left:0px; top:200px;}
-  100% {left:400px; top:300px;}
+.textarea-height {
+  height: 200px;
 }
 </style>
 <script>
@@ -49,13 +36,13 @@ export default {
         return {
             textToUwuIfy: '',
             showNotification: false,
-            copyElement: '',
         };
     },
 
     computed: {
         textUwuFied: function () {
-            let smileyTextArray = ['UWU', 'OWO', '*aaaargg*', '*wdf!*']
+            this.showNotification = false;
+            let smileyTextArray = ['UWU', 'OWO', '*aaaargg*', '*wdf!*', '*Hello there!*', '*Wowiepowie*', '*oi oi oi*', '*tjonge*', ':P', ':o', ':)', 'XD']
             let text = this.textToUwuIfy.replace(/r|l/g, 'w')
                 .replace(/L|R/g, 'W')
                 .replaceAll('Na', 'Nya')
@@ -103,13 +90,10 @@ export default {
         saveToClipboard() {
             this.showNotification = true;
 
-            let itemToCopy = document.getElementById("uwuout").value;
-            // copyText.select();
-            // copyText.setSelectionRange(0, 99999);
-            // console.log(copyText.value);
-            // window.navigator.clipboard.writeText(copyText.value);
-
-            this.$parent.saveToClipboard(itemToCopy);
+            var input = document.getElementById("uwuout");
+            input.select(),
+            input.setSelectionRange(0,input.value.length),
+            document.execCommand("copy");
         },
     },
 
