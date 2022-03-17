e159ebdd845fad3be565a42ab54c8dcd4910d613
Piet <pkorfmaker@gmail.com>
2022-03-17 14:09:34 +0100

    Create play steam game button

--- resources/views/steam/show.blade.php
+++ resources/views/steam/show.blade.php
@@ -45,18 +45,17 @@
                         <div class="col-md-8">
                             <div class="card-body pb-0">
                                 @if (isset($selectedGameInfo['name']))
-                                    <h5 class="card-title">{{ $selectedGameInfo['name'] }} | speeltijd: {{ $selectedGameInfo['playtime_forever'] }}</h5>
+                                    <h5 class="card-title hidden-icon text-center"><a href="steam://run/{{$selectedGameInfo['steam_appid']}}/"> <i class="fas fa-play icon"></i>{{ $selectedGameInfo['name'] }}</a> | speeltijd: {{ $selectedGameInfo['playtime_forever'] }}</h5>
                                 @else
-                                    <h5 class="card-title">Dit spel heeft geen naam</h5>
+                                    <h5 class="card-title text-center">Dit spel heeft geen naam</h5>
                                 @endif
                                 @if (isset($selectedGameInfo['short_description']))
-                                    <h class="card-title">{{ $selectedGameInfo['short_description'] }} | speeltijd: {{ $selectedGameInfo['playtime_forever'] }}</h>
+                                    <p class="card-title">{{ $selectedGameInfo['short_description'] }}}</p>
                                 @else
-                                    <h class="card-title">Dit spel heeft geen beschrijving</h>
+                                    <p class="card-title">Dit spel heeft geen beschrijving</p>
                                 @endif
                             </div>
                         </div>
-                        <Submit-button></Submit-button>
                         <form action="{{ route('steam.getNewGame', $user) }}" class="p-3">
                             <div class="input-group mb-3">
                                 <span class="input-group-text bg-primary border-primary">Tussen de</span>
@@ -74,14 +73,14 @@
                     <div class="row g-0">
 
                         <div class="card-body m-0">
-                            @if (isset($selectedGameInfo['name']))
+                            @if (!isset($selectedGameInfo['name']) || $user->getKey() !== Auth()->user()->id)
+                            <h5 class="card-title">Voor dit spel kan je geen feedback geven</h5>
+                            @else
                                 <div class="mb-3">
                                     <label for="feedback" class="form-label">Wat vond je van {{ $selectedGameInfo['name'] }}?</label>
                                     <textarea class="form-control" name="feedback" id="feedback" rows="3" placeholder="Ik vind {{ $selectedGameInfo['name'] }}..."></textarea>
                                 </div>
                                 <button class="btn btn-outline-primary w-100">Verstuur</button>
-                            @else
-                            <h5 class="card-title">Voor dit spel kan je geen feedback geven</h5>
                             @endif
                         </div>
                     </div>
@@ -95,7 +94,7 @@
                                 <div class="card w-100 bg-dark">
                                 <img src="{{ $game['img_logo_url'] }}" class="card-img-top" alt="{{ $game['name'] }}">
                                 <div class="card-body">
-                                    <h5 class="card-title">{{ $game['name'] }}</h5>
+                                    <h5 class="card-title hidden-icon text-center"><a href="steam://run/{{$game['appid']}}/"><i class="fas fa-play icon"></i>{{ $game['name'] }}</a></h5>
                                     <hr class="mt-0 mb-2">
                                     <p class="mb-0">Afgelopen 2 weken {{ App\Services\Steam::minutesToHours($game['playtime_2weeks']) }} gespeeld</p>
                                     <p>In totaal {{ App\Services\Steam::minutesToHours($game['playtime_forever']) }} gespeeld</p>
