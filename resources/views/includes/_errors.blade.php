c8995de440fbcd94dad6a1c7119419bb52495c8b
Piet <pkorfmaker@gmail.com>
2022-04-07 13:26:58 +0200

    Fix errors blade

--- resources/views/includes/_errors.blade.php
+++ resources/views/includes/_errors.blade.php
@@ -1,4 +1,4 @@
-@php $bag = (isset($bag)) ? $bag : ''; @endphp
+{{-- @php $bag = (isset($bag)) ? $bag : ''; @endphp
 
 @if ($errors->{$bag}->any())
 
@@ -12,4 +12,12 @@
             @endforeach
         </ul>
     </div>
+@endif --}}
+
+@if(count($errors) > 0)
+    <ul class="list-group mb-3">
+        @foreach ($errors->all() as $error)
+            <div class="list-group-item list-group-item-danger">{{ $error }}</div>
+        @endforeach
+    </ul>
 @endif
