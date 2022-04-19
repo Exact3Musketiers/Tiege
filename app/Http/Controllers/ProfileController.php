843ccb819645f73e9e1e6696d78d3b4b9be39282
PietJK <37580277+PietJK@users.noreply.github.com>
2022-04-19 10:10:30 +0200

    Fix steam user delete bug
    When user with steam reviews wanted to delete profile they couldn't because of foreign keys

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\SteamReview;
 use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
@@ -61,6 +62,7 @@ class ProfileController extends Controller
             abort(403);
         }
 
+        SteamReview::whereUserId($profile->getKey())->delete();
         $profile->delete();
         return view('auth.login');
     }
