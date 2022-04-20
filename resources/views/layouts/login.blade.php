ef03d5ddb0104fd09d56c2cbea829507a4703991
Nanco Feenstra <48758689+Nancofeenstra@users.noreply.github.com>
2022-04-20 10:33:48 +0200

    Update login.blade.php

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -26,7 +26,7 @@
     <div class="row w-100 h-100 m-0">
         <div class="col-xs-12 col-md-6 bg-dark h-100">
             <div class="row h-100 align-items-center justify-content-center">
-                <div class="col-md-8 fs-5">
+                <div class="col-md-8 fs-5 bg-dark">
                     @yield('content')
                 </div>
             </div>
