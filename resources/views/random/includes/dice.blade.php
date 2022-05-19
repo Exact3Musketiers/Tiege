1597dd00512bf5aa3d441faa124ba3fd540b6d7b
Piet <pkorfmaker@gmail.com>
2022-05-19 21:50:06 +0200

    Create/update all random thingies

--- resources/views/random/includes/dice.blade.php
+++ resources/views/random/includes/dice.blade.php
@@ -1,12 +1,45 @@
-<div class="row mt-5">
+<div class="row mt-3" id="dicePage">
     <div class="card bg-dark col-md-12">
-        <div class="card-header">Rol een dobbelsteentje</div>
         <div class="card-body">
-            <div class="btn btn-warning" onclick="rollDice">Dice Dice DICE!</div>
+            <button class="btn btn-primary w-100" onclick="rollDice();">Rol een dobbelsteen</button>
+            <div class="dice-arena mt-3 d-flex flex-column justify-content-center">
+                <h1 class="m-0 text-center" id="dice">Gooi een dobbelsteen</h1>
+            </div>
         </div>
     </div>
 </div>
 
 <script>
+    function rollDice() {
+        let dice = document.getElementById('dice');
+        let number = getRandomDice();
+        dice.innerHTML = number;
+    }
 
+   function getRandomDice() {
+        let dice;
+        let random = Math.floor(Math.random() * 6) + 1;
+        switch (random) {
+            case 1:
+                dice = '<i class="fas fa-dice-one rollDice"></i>';
+                break;
+            case 2:
+                dice = '<i class="fas fa-dice-two rollDice"></i>';
+                break;
+            case 3:
+                dice = '<i class="fas fa-dice-three rollDice"></i>';
+                break;
+            case 4:
+                dice = '<i class="fas fa-dice-four rollDice"></i>';
+                break;
+            case 5:
+                dice = '<i class="fas fa-dice-five rollDice"></i>';
+                break;
+            case 6:
+                dice = '<i class="fas fa-dice-six rollDice"></i>';
+                break;
+        }
+
+        return dice;
+   }
 </script>
--- resources/views/random/includes/rng.blade.php
+++ resources/views/random/includes/rng.blade.php
@@ -1,9 +1,53 @@
 
-<div class="row mt-5">
+<div class="row mt-3" id="rngPage">
     <div class="card bg-dark col-md-12">
-        <div class="card-header">Pak een lekker getalletje</div>
-        <div class="card-body">
-            <p>rng</p>
+        <div class="card-body text-center">
+            <div class="input-group mb-3">
+                <span class="input-group-text bg-primary border-primary"">#1</span>
+                <input type="number" id="numberInput1" class="form-control" placeholder="Number 1" aria-label="Number 1">
+                <span class="input-group-text bg-primary border-primary">#2</span>
+                <input type="number" id="numberInput2" class="form-control" placeholder="Number 2" aria-label="Number 2">
+            </div>
+            <button id="rngButton" class="btn btn-primary mb-3 w-100" onclick="rng();">Pak een lekker random getalletje</button>
+            <div class="rng-arena p-3 pt-2 mx-auto d-flex flex-column justify-content-center">
+                <h1 id="rngText" class="h4 m-0"></h1>
+                <h1 id="drum" class="m-0">Random nummer</h1>
+                <h1 id="numberText" class="m-0"></h1>
+            </div>
         </div>
     </div>
 </div>
+
+<script>
+    function rng() {
+        let drum = document.getElementById('drum');
+        let rngText = document.getElementById('rngText');
+        let rngButton = document.getElementById('rngButton');
+        let numberText = document.getElementById('numberText');
+        let input1 = document.getElementById('numberInput1');
+        let input2 = document.getElementById('numberInput2');
+
+        if (input1.value == '' || input2.value == '') {
+            alert('Niet vergeten een getal in te vullen')
+            return;
+        }
+        if (input1.value >= input2.value) {
+            alert('Het eerste getal kan niet hoger zijn dan het laatste getal')
+            return;
+        }
+
+        numberText.innerHTML = '';
+        rngText.innerHTML = 'En je getalletje is......';
+        drum.innerHTML = '<i class="fas fa-drum animateDrum"></i>';
+        rngButton.disabled = true;
+
+        setTimeout(() => {
+            drum.innerHTML = '';
+
+            let random = Math.floor(Math.random() * (input2.value - input1.value + 1)) + parseInt(input1.value);
+            numberText.innerHTML = random;
+
+            rngButton.disabled = false;
+        }, 2000);
+    }
+</script>
