2a61ba375d548f839aee79ebdf92921f260da108
Piet <pkorfmaker@gmail.com>
2023-03-13 17:16:37 +0100

    SMall styling fix

--- resources/views/wiki/show.blade.php
+++ resources/views/wiki/show.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="w-100">
+<div class="w-100 p-3 text-center text-secondary">
     <nav aria-label="breadcrumb">
         <h1>{{ $wiki->start }} <i class="fas fa-long-arrow-alt-right"></i> {{ $wiki->end }}</h1>
-      </nav>
+    </nav>
 </div>
 <div class="container">
     <div class="py-3">
