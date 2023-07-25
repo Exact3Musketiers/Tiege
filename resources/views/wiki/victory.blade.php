99116e58b7d2111568edc09b9ef2ae73fcc4ce4c
Pietjk <piet@postisit.nl>
2023-07-25 16:05:35 +0200

    Make victory beautifull

--- resources/views/wiki/victory.blade.php
+++ resources/views/wiki/victory.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="py-3">
-        <h1>Gelukt!!! Je bent er!</h1>
-        <h2>Je hebt er {{ $count }} stappen over gedaan.</h2>
-        <h3>{{ $wiki->start }} <i class="fas fa-long-arrow-alt-right"></i> {{ $wiki->end }}</h3>
-
+<div class="container text-center confetti">
+    <div class="d-flex flex-col min-vh-100 align-items-center">
+        <div class="py-3 mx-auto">
+            <h1 class="rainbow_text_animated">Gelukt!!! Je bent er!</h1>
+            <h2>Je hebt er {{ $count }} stappen over gedaan.</h2>
+            <h3>{{ $wiki->start }} <i class="fas fa-long-arrow-alt-right"></i> {{ $wiki->end }}</h3>
+        </div>
     </div>
 </div>
+@endsection()
